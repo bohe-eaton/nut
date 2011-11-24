0e3f89af5af226076394d06ea8f06e3f1fd4c2cb
Frederic Bohe <fbohe-guest@alioth.debian.org>
2011-11-24 08:56:56 +0000

    Re-create Eaton_SDK branch with (hopefully) rights files properties.
    Fossil-ID: SVN r3330

--- drivers/usbhid-ups.c
+++ drivers/usbhid-ups.c
@@ -42,7 +42,7 @@
 /* include all known subdrivers */
 #include "mge-hid.h"
 
-#ifndef SHUT_MODE
+#ifndef EATON_MODE
 	#include "explore-hid.h"
 	#include "apc-hid.h"
 	#include "belkin-hid.h"
@@ -56,11 +56,11 @@
 
 /* master list of avaiable subdrivers */
 static subdriver_t *subdriver_list[] = {
-#ifndef SHUT_MODE
+#ifndef EATON_MODE
 	&explore_subdriver,
 #endif
 	&mge_subdriver,
-#ifndef SHUT_MODE
+#ifndef EATON_MODE
 	&apc_subdriver,
 	&belkin_subdriver,
 	&cps_subdriver,
