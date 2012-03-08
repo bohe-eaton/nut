5b9444c451bf98343c5b7cbcfbf5d6a268d4476f
Frederic Bohe <fbohe-guest@alioth.debian.org>
2012-03-08 14:37:33 +0000

    Clean up
    Fossil-ID: SVN r3492

--- clients/upsclient.c
+++ clients/upsclient.c
@@ -29,14 +29,12 @@
 #endif
 
 #include <errno.h>
-#ifndef WIN32
-#include <netdb.h>
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #ifndef WIN32
+#include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
