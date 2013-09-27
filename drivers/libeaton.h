7027a2650ea2a64bb542618401370a48d7eaec74
Frédéric BOHE <fredericbohe@eaton.com>
2013-09-27 10:36:51 +0200

    Cosmetic changes

--- drivers/libeaton.h
+++ drivers/libeaton.h
@@ -1,7 +1,7 @@
 /* libeaton.h - Header for Eaton SDK
  * Copyright (C) 2011 Eaton
  *  Author: Frederic BOHE <fredericbohe@eaton.com>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
